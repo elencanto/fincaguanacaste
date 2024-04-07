b7334a7b59931b4d78e400e82c6bca4045ba87ae
Joel Sanchez <joelsn82@gmail.com.com>
2024-04-06 18:01:27 -0600

    general updates

--- script.js
+++ script.js
@@ -7,47 +7,16 @@ function openModal(imagePath) {
     modalImg.src = imagePath;        
   }
   
-  // Función para cerrar el modal
-  function closeModal() {
-    const modal = document.getElementById("imageModal");
-    modal.style.display = "none";
+// Función para cerrar el modal
+function closeModal() {
+  const modal = document.getElementById("imageModal");
+  modal.style.display = "none";
   }
   
-  // Cerrar el modal si el usuario hace clic fuera de la imagen
-  window.onclick = function(event) {
-    const modal = document.getElementById("imageModal");
-    if (event.target === modal) {
-      modal.style.display = "none";
+// Cerrar el modal si el usuario hace clic fuera de la imagen
+window.onclick = function(event) {
+  const modal = document.getElementById("imageModal");
+  if (event.target === modal) {
+    modal.style.display = "none";
     }
   }
-
-  let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
